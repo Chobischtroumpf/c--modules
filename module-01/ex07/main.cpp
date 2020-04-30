8d5a0e739acc7014023dc35cde390ed922381106
adorigo <adorigo@student.s19.be>
2020-04-30 14:37:16 +0200

    finished the replace ex

--- module-01/ex07/main.cpp
+++ module-01/ex07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adorigo <adorigo@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 12:35:27 by adorigo           #+#    #+#             */
-/*   Updated: 2020/04/30 12:26:25 by adorigo          ###   ########.fr       */
+/*   Updated: 2020/04/30 14:34:29 by adorigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ int	ft_replace(const char *filename, std::string str_find, std::string str_repla
 {
 	std::ostringstream text;
 	size_t start_pos = 0;
-	std::ofstream in_file(filename);
+	std::ifstream in_file(filename);
 	text << in_file.rdbuf();
 	std::string str = text.str();
 	
-	while((start_pos = str.find(str_find, start_pos)) != std::string::npos) {
+	while((start_pos = str.find(str_find, start_pos)) != std::string::npos) 
+	{
 		str.replace(start_pos, str_find.length(), str_replace);
-		start_pos += str_replace.length(); // Handles case where 'to' is a substring of 'from'
+		start_pos += str_replace.length();
 	}
 	
 	std::stringstream string;
@@ -38,14 +39,20 @@ int	ft_replace(const char *filename, std::string str_find, std::string str_repla
 int	main(int argc, char const *argv[])
 {
 	if (argc != 4)
-		std::cout << "there aren't four args";
-	else if (!argv[2] && !argv[3])
-		std::cout << "str1 or 2 are empty";
-	else
 	{
-		if (!ft_replace(argv[1], argv[2], argv[3]))
-			std::cout << "ft_replace failed";
+		std::cout << "there aren't four args";
+		return 0;
 	}
-	 
+	else if (!argv[2] && !argv[3])
+	{
+		std::cout << "str1 or 2 are empty";
+		return 0;
+	}
+	else
+		if (!ft_replace(argv[1], argv[2], argv[3]))
+		{
+			std::cout << "ft_replace failed";
+			return 0;
+		}
 	return 0;
 }
